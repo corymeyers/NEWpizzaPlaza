5e777deb2d99a7bb5217bb47192ea8ac7dbfc61f
cory meyers <whippoorwill.in.flight@gmail.com>
2016-02-02 12:51:24 -0800

    changwed random code

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,19 @@ function Pizza(elSize, elToppings, elTabulator) {
 
 Pizza.prototype.elToppings = function() {
   var toppingSum = 0;
-  if(document.getElementById("Pepperoni").checked) {
+  if($(document.getElementById("Pepperoni").checked)) {
     toppingSum += 1;
   }
-  if(document.getElementById("Pickles").checked) {
+  if($(document.getElementById("Pickles").checked)) {
     toppingSum += 4;
   }
-  if(document.getElementById("Porridge").checked) {
+  if($(document.getElementById("Porridge").checked)) {
     toppingSum += 9;
   }
-  if(document.getElementById("Peppercini").checked) {
+  if($(document.getElementById("Peppercini").checked)) {
     toppingSum += 5;
   }
-  if(document.getElementById("Potato").checked) {
+  if($(document.getElementById("Potato").checked)) {
     toppingSum += 2;
   }
   return toppingSum;
@@ -27,13 +27,13 @@ Pizza.prototype.elToppings = function() {
 
 Pizza.prototype.elSize = function() {
   var sizeSum = 0;
-  if(document.getElementById("Small").checked) {
+  if($(document.getElementById("Small").checked)) {
     toppingSum += 10;
   }
-  if(document.getElementById("Medium").checked) {
+  if($(document.getElementById("Medium").checked)) {
     toppingSum += 15;
   }
-  if(document.getElementById("Large").checked) {
+  if($(document.getElementById("Large").checked)) {
     toppingSum += 20;
   }
   return sizeSum;
@@ -47,7 +47,9 @@ $(document).ready(function() {
   $("form#pizzaForm").submit(function(event) {
     event.preventDefault();
     var elPizza = new Pizza(elSize, elToppings, elTabulator);
-    var elPrice = elPizza.elTabulator();
+    var elSize = this.elSize
+    var elToppings = this.elToppings
+    var elTabulator = this.elTabulator
 
     $("span#elPrice").text(elPrice);
 
