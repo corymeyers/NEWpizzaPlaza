77205bad25c3344d578c4b024330ac04ae37de9b
cory meyers <whippoorwill.in.flight@gmail.com>
2016-02-03 22:30:52 -0800

    add loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,39 @@
 function Pizza(elSize, elToppings) {
   this.elSize = elSize;
   this.elToppings = elToppings;
+  this.elPrice = 0;
 }
 
 Pizza.prototype.elToppingsCaculator = function() {
-  var toppingSum = 0;
-  if (this.pepperoni === true) {
-    return toppingSum += 4;
+  this.elPrice = 0;
+  for (var i = 0; i < this.toppings.length; i++) {
+    this.elPrice += 1;
   }
-  if (this.pickles === true) {
-    toppingSum += 5;
-  }
-  if (this.porridge === true) {
-    toppingSum += 6;
-  }
-  if (this.peppercini === true) {
-    toppingSum += 9;
-  }
-  if (this.potato === true) {
-    toppingSum += 7;
-  }
-  return toppingSum;
 }
 
 Pizza.prototype.elSizeCaculator = function() {
-  var sizeSum = 0;
-  if(this.small === true) {
-    sizeSum += 6;
-  }
-  if(this.medium === true) {
-    sizeSum += 9;
-  }
-  if(this.large === true) {
-    sizeSum += 7;
-  }
-  return sizeSum;
+  this.elPrice = 0;
+  if(this.size === "small") {
+    this.elPrice =+ 14;
+  } else if (this.size === "medium") {
+    this.elPrice =+ 16;
+  } else if (this.size === "large") {
+    this.elPrice =+ 18;
+  } return this.elPrice
 }
 
+
 Pizza.prototype.elTabulator = function() {
-  return this.elSize + this.elToppings;
+  return this.elPrice;
 }
 
 $(document).ready(function() {
   $("form#pizzaForm").submit(function(event) {
     event.preventDefault();
-    var pepperoni = $("#pepperoni").is(":checked");
-    var pickles = $("#pickles").is(":checked");
-    var porridge = $("#porridge").is(":checked");
-    var peppercini = $("#peppercini").is(":checked");
-    var potato = $("#potato").is(":checked");
-    var small = $("#small").is(":checked");
-    var medium = $("#medium").is(":checked");
-    var large = $("#large").is(":checked");
-    if (small === true) {
-      elSize = small;
-    } else if
-
+    var elToppings = $('input:checkbox[name=toppings]:checked')
+    var elSize = $('input[name=size]:checked').val();
     var newPizza = new Pizza(elSize, elToppings);
-    var elSize = ""
-    var elToppings = ""
         // document.getElementById("elPrice").innerHTML = elTabulator();
-    $('.elPrice').text(newPizza.elTabulator());
+    $('.recipt').text(newPizza.elTabulator());
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
-describe("PizzaOrder", function() {
-  var testPizzaOrder = new PizzaOrder(1, "large", 14);
-
-  it ("collects user input, and finds the value", function() {
-    expect(testPizzaOrder.PizzaToppings("Pepperoni")).to.equal(1);
+describe("Pizza", function() {
+  it ("collects user input, and makes it into an object", function() {
+    var testPizza = new Pizza("large", ["pepperoni", "pickles"]);
+    expect(testPizza.elSize).to.equal("large");
+    expect(testPizza.elToppings).to.eql["pepperoni", "pickles"];
   });
-
+it ("it takes that input and assigns pricing", function() {
+    var testPizza = new Pizza("large", ["pepperoni", "pickles"]);
+    expect
+});
 
 });
+//
